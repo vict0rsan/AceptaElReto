04c22b77bb90071eb2f604b64d55428f0590feac
Víctor Santiago de Mena <vict0rsan@Victors-MacBook-Pro.local>
2021-06-25 17:20:44 +0200

    Problema 535

---
+++ src/volumen5/problema535.java
@@ -0,0 +1,29 @@
+package volumen5;
+import java.util.*;
+
+public class problema535 {
+
+    public static void main (String [] args){
+
+        Scanner input = new Scanner(System.in);
+
+        while(true){
+
+            int tramos = input.nextInt();
+
+            if(tramos == 0){
+                break;
+            }
+
+            int [] alturas = new int[tramos];
+
+            for(int i = 0; i < tramos; i++){
+                alturas[i] = input.nextInt();
+            }
+
+
+
+        }
+
+    }
+}
