b635d59b6f524045f50a3f426d73c4d2b53b2e8a
victorsan <32201744+vict0rsan@users.noreply.github.com>
2021-06-27 00:44:11 +0200

    Update problema605.java

--- src/volumen6/problema605.java
+++ src/volumen6/problema605.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class problema605 {
 	
-	public static void main(String [] args) {
+	public static void main(String [] args) {  //PERFECTO
 		
 		Scanner input = new Scanner(System.in);
 		
