4923129fa3d9616c604aaf4c0b9b8708adaa38db
vict0rsan <vicloligye@hotmail.com>
2021-06-27 00:42:27 +0200

    Problema 605

---
+++ src/volumen6/problema605.java
@@ -0,0 +1,44 @@
+package volumen6;
+
+import java.util.Scanner;
+
+public class problema605 {
+	
+	public static void main(String [] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		while(true) {
+			
+			int verano = 0;
+			int invierno = 0;
+			char c = input.next().charAt(0);
+			
+			if(c == '.')
+				break;
+			
+			do {
+				
+				if(c == 'V')
+					verano++;
+				if(c == 'I')
+					invierno++;
+				c = input.next().charAt(0);
+				
+				
+			}while(c != '.');
+			
+			if(verano > invierno)
+				System.out.println("VERANO");
+			else if(invierno > verano)
+				System.out.println("INVIERNO");
+			else
+				System.out.println("EMPATE");
+			
+			
+			
+		}
+	}
+	
+
+}
