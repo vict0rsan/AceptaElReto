b40a2452dfc65eedb8966ca1029ea7974be884c0
Víctor Santiago de Mena <vict0rsan@Victors-MacBook-Pro.local>
2021-06-28 14:52:53 +0200

    Problema 533

---
+++ src/volumen5/problema533.java
@@ -0,0 +1,54 @@
+package volumen5;
+import java.util.*;
+
+public class problema533 {
+
+    public static void main (String [] args){ //PERFECTO
+
+        Scanner input = new Scanner(System.in);
+
+        int casos = input.nextInt();
+
+        for(int i = 0; i < casos; i++){
+
+            double sumaKilos = 0;
+            boolean objetivo = false;
+            int personas = 1;
+            int kilosDeseados = input.nextInt();
+
+            while(true){
+
+
+                int numeroBotellas = input.nextInt();
+
+                if(numeroBotellas == 0)
+                    break;
+                else {
+
+                    sumaKilos += numeroBotellas * 0.125;
+
+                    if (sumaKilos >= kilosDeseados) {
+                        objetivo = true;
+                        input.nextLine();
+                        break;
+                    }
+
+                    personas++;
+
+                }
+
+            }
+            //System.out.println("Iteracion numero: " + i);
+
+            if(objetivo)
+                System.out.println(personas);
+            if(!objetivo)
+                System.out.println("SIGAMOS RECICLANDO");
+
+
+        }
+
+
+
+    }
+}
