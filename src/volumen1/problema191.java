a7fbb7b761cfd6237c2ea4c633c76fe2103669f6
vict0rsan <vicloligye@hotmail.com>
2021-07-15 23:53:35 +0200

    Problema 191

--- src/volumen1/problema191.java
+++ src/volumen1/problema191.java
@@ -2,25 +2,21 @@
 package volumen1;
 import java.util.Scanner;
 
-public class problema191 {
+public class problema191 { //PERFECTO - ACCEPTED
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce el numero de casos de prueba: ");
         int casos = input.nextInt();
         int volumen = 0;
         
         for(int i=0; i<casos; i++){
-            System.out.println("Introduce compartimentos del acuario: ");
             int compartimentos = input.nextInt();
-            System.out.println("Introduce el volumen del mayor compartimento ");
             int mayorCapacidad = input.nextInt();
-            System.out.println("Introduce la diferencia de volumen entre compartimentos ");
             int diferenciaCapacidad = input.nextInt();
             
             for(int j=0; j<compartimentos; j++){
                 volumen = volumen + mayorCapacidad - j*diferenciaCapacidad;
             }
-            System.out.println("Volumen: "+volumen);
+            System.out.println(volumen);
             volumen = 0;
             
         }
