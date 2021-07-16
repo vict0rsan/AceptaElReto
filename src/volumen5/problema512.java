a3719c99a8c7baa611030e6246c7bee196ee2a45
victorsan <32201744+vict0rsan@users.noreply.github.com>
2021-07-16 23:55:08 +0200

    Problema 512

--- src/volumen5/problema512.java
+++ src/volumen5/problema512.java
@@ -1,22 +1,24 @@
-
 package volumen5;
 import java.util.Scanner;
 
 
-public class problema512 {
+public class problema512 { //PERRFECTO - ACCEPTED
+
     public static void main (String[]args){
-        System.out.println("Introduce el número de casos de prueba: ");
+
         Scanner input = new Scanner(System.in);
         int casos = input.nextInt();
         
         for(int i = 0; i < casos; i++){
-            System.out.println("Introduce los kg de caballo que vas a introducir: ");
+
             int conejo = input.nextInt();
-            System.out.println("Introduce los kg de conejo que vas a introducir: ");
             int caballo = input.nextInt();
             int pconejo = Math.round((conejo*100)/(conejo+caballo));
-            System.out.println("Porcentaje de conejo en la mezcla: " + pconejo + "%");
+
+            System.out.println(pconejo);
+
         }
     }
     
 }
+
