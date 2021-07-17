7e5cc2caee2c294651f51c828dbfb682b926ab03
vict0rsan <vicloligye@hotmail.com>
2021-07-17 15:41:34 +0200

    Problema 219

---
+++ src/volumen2/problema219.java
@@ -0,0 +1,30 @@
+package volumen2;
+
+import java.util.Scanner;
+
+public class problema219 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int casos = input.nextInt();
+
+        for(int i = 0; i < casos; i++){
+
+            int comprados = 0;
+
+            int decimos = input.nextInt();
+
+            for(int j = 0; j < decimos; j++){
+                int numero = input.nextInt();
+
+                if(numero%2==0)
+                    comprados++;
+
+            }
+
+            System.out.println(comprados);
+        }
+    }
+}
