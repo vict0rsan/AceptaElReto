0cbe8f1f9b82506277ec7c0fe8fbbe899826c26c
vict0rsan <vicloligye@hotmail.com>
2021-07-30 15:10:52 +0200

    Problema 232

--- src/volumen2/problema232.java
+++ src/volumen2/problema232.java
@@ -1,53 +1,42 @@
 package volumen2;
 
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
-public class problema232 {
+public class problema232 { //PERFECTO - ERROR POR USAR API DE JAVA 8
 
-    public static void main(String[] args) {  //comrpobar esta mas o menos la estructura
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int datos[] = new int[6];
+        int fechas[] = new int[6];
         int[] days = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        do {
-            int diasM = 0;
-            for (int i = 0; i < 6; i++) {
-                System.out.println("Introduce dato nº = " + i);
-                datos[i] = input.nextInt();
-            }
-            if(datos[0] == 0 && datos[1] == 0 && datos[2] == 0 &&
-                    datos[3] == 0 && datos[4] == 0 && datos[5] == 0)
+        while(true) {
+
+            for(int i = 0; i < 6; i++)
+                fechas[i] = input.nextInt();
+
+            if (fechas[0] == 0 && fechas[1] == 0 && fechas[2] == 0 && fechas[3] == 0 && fechas[4] == 0 && fechas[5] == 0)
                 break;
-            
-            if(datos[4]-datos[1]<0){
-                for(int i = datos[1]-1; i < days.length; i++){
-                    diasM+=days[i];
-                    System.out.println("de x hasta el final: "+diasM);
-                    System.out.println("i: " + i);
-                    System.out.println("days.length: " + days.length);
-                }
-                for(int j = 0; j < datos[4]-1;j++){
-                    diasM+=days[j];
-                    System.out.println("de 0 hasta y: " + diasM);
-                }
-                System.out.println("dias solo teniendo en cuenta los meses = " + diasM);
-            }else if(datos[4]-datos[1]>0){
-                for(int i = datos[1]-1; i < datos[4]-1; i++){
-                    diasM+=days[i];
-                }
-                System.out.println("dias solo teniendo en cuenta los meses = " + diasM);
+
+            LocalDate bornDate = LocalDate.of(fechas[2],fechas[1],fechas[0]);
+            LocalDate currentDate = LocalDate.of(fechas[5],fechas[4],fechas[3]);
+
+            long daysElapsed = ChronoUnit.DAYS.between(bornDate, currentDate);
+
+            int diasCumpleaños = fechas[5] - fechas[2];
+            int diasBisiestos = diasCumpleaños/4;
+            int extra = 0;
+
+            if(fechas[4] <= fechas[1]){
+                if(fechas[3] < fechas[0])
+                    extra--;
             }
-          diasM = diasM-datos[0]+datos[3];
-            
-            System.out.println("dias tras tener en cuenta los dias tmb = " + diasM);
-            
-            diasM = diasM + (datos[5]-datos[2]-1)*365;
-            System.out.println(" dias tras añadir años = " +  diasM);
-            
-            System.out.println(diasM);
-            
-        } while (true);
 
+            if(daysElapsed == 365)
+                System.out.println(0);
+            else
+                System.out.println(daysElapsed - diasCumpleaños - diasBisiestos - extra);
+        }
     }
-
 }
