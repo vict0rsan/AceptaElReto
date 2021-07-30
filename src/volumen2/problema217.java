7ef938cb037bfeebb89b7f93e71fd05fab1ab084
vict0rsan <vicloligye@hotmail.com>
2021-07-30 14:31:01 +0200

    Problema 217

--- src/volumen2/problema217.java
+++ src/volumen2/problema217.java
@@ -3,7 +3,7 @@ package volumen2;
 import java.util.Scanner;
 
 
-public class problema217 {
+public class problema217 { //PERFECTO - ACCEPTED
     public static void main(String[] args) {
         
         Scanner s = new Scanner(System.in);
