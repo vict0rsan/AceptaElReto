b9cae08cbd2d1c70d5201477514b172e4af4094f
vict0rsan <vicloligye@hotmail.com>
2021-08-01 00:12:29 +0200

    Problema 532

--- src/volumen5/problema532.java
+++ src/volumen5/problema532.java
@@ -1,24 +1,17 @@
-
 package volumen5;
 import java.util.Scanner;
 
-
 public class problema532 {
+
     public static void main(String[]args){
+
         Scanner input = new Scanner(System.in);
-        System.out.println("¿Cuántos casos quieres evaluar?");
         int casos = input.nextInt();
         
         for( int i = 0; i < casos; i++){
-            System.out.println("Introduce el peso neto del producto: ");
             int nproducto = input.nextInt();
-            System.out.println("Introduce el peso total del producto: ");
             int tproducto = input.nextInt();
-            System.out.println("Peso del envase: " + (tproducto - nproducto));
-            
+            System.out.println(tproducto - nproducto);
         }
-        
-        
     }
-    
 }
