af4be9453d1ab65c240abc1896c0cc9993f0f7fa
vict0rsan <vicloligye@hotmail.com>
2021-07-31 16:38:54 +0200

    Problema 313

--- src/volumen3/problema313.java
+++ src/volumen3/problema313.java
@@ -1,30 +1,20 @@
 package volumen3;
 import java.util.Scanner;
 
-public class problema313 { //PERFECTO
+public class problema313 { //PERFECTO - ACCEPTED
+
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce numero de casos: ");
         int casos = input.nextInt();
         
         for(int i = 0; i < casos; i++){
-            boolean check = true;
-            System.out.println("Introduce dinero en la cuenta: ");
+
             int inicio = input.nextInt();
-            System.out.println("Introduce ingresos - gastos durante el mes: ");
             int fin = input.nextInt();
-            
-            if((inicio<0) ){
-                if(fin < 0){
-                    check = false;
-                }
-            }
-            else if(inicio>=0){
-                if(inicio+fin<0)
-                    check = false;
-            }
+            boolean check = inicio + fin >= 0;
+
             System.out.println(check?"SI":"NO");
         }
     }
-    
 }
