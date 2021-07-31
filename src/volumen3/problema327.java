b6375ae93321a1ccf7700c5c6a2655148cafe0d1
vict0rsan <vicloligye@hotmail.com>
2021-07-31 16:57:41 +0200

    POR RESOLVER

--- src/volumen3/problema327.java
+++ src/volumen3/problema327.java
@@ -1,25 +1,19 @@
-
 package volumen3;
 import java.util.*;
 
+public class problema327 { //POR RESOLVER
 
-public class problema327 {
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
-        
-        
         
         while(true){
             
-            long numero = s.nextLong();
+            int numero = s.nextInt();
             
             if(numero==0)
                 break;
-            
-            
+
         }
-        
-        
     }
-    
 }
