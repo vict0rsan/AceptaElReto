263876d396c4485a8550d8ae729f717a4a6fc601
vict0rsan <vicloligye@hotmail.com>
2021-07-31 15:32:55 +0200

    Problema 247

--- src/volumen2/problema247.java
+++ src/volumen2/problema247.java
@@ -4,29 +4,33 @@ import java.util.Scanner;
 
 public class problema247 {
 
-    public static void main(String[] args) { //PERFECTO
+    public static void main(String[] args) { //PERFECTO - ACCEPTED
+
         Scanner input = new Scanner(System.in);
 
-        do {
-            System.out.println("Introduce numero de muestras: ");
-            int m = input.nextInt();
-            if(m==0)
-                break;
-            int[] datos = new int[m];
-            boolean check = true;
-            for (int i = 0; i < m; i++) {
-                System.out.println("Introduce valor: " + (i + 1));
-                datos[i] = input.nextInt();
-            }
+        while (true) {
 
-            for (int j = 0; j < m - 1; j++) {
+            int muestras = input.nextInt();
+
+            if(muestras==0)
+                break;
+
+            int[] datos = new int[muestras];
+            boolean check = true;
+
+            for (int i = 0; i < muestras; i++)
+                datos[i] = input.nextInt();
+
+
+            for (int j = 0; j < muestras - 1; j++) {
                 if (datos[j + 1] <= datos[j]) {
                     check = false;
                 }
             }
 
             System.out.println(check ? "SI" : "NO");
-        } while (true);
+
+        }
     }
 
 }
