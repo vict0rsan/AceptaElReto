62e0a57b9d5a987193d8895650999b2c7d1d02b4
vict0rsan <vicloligye@hotmail.com>
2021-07-31 16:53:00 +0200

    Problema 314

--- src/volumen3/problema314.java
+++ src/volumen3/problema314.java
@@ -1,36 +1,33 @@
 package volumen3;
 import java.util.Scanner;
 
-
 public class problema314 { //PERFECTO
+
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce casos de prueba: ");
         int casos = input.nextInt();
         
         for(int i = 0; i < casos; i ++){
             int picos = 0;
             int valles = 0;
             
-            System.out.println("Introduce numero de muestras: ");
-            int m = input.nextInt();
-            int [] temp = new int [m];
+            int numeroTemperaturas = input.nextInt();
+            int [] temperaturas = new int [numeroTemperaturas];
             
-            for(int j = 0; j < temp.length; j++){
-                System.out.println("Introduce dato numero: " + (j+1));
-                temp[j]=input.nextInt();
+            for(int j = 0; j < temperaturas.length; j++){
+                temperaturas[j] = input.nextInt();
             }
             
-            for(int k = 1; k < temp.length-1; k++){
-                if(temp[k]>temp[k+1]&&temp[k]>temp[k-1])
+            for(int k = 1; k < temperaturas.length-1; k++){
+                if(temperaturas[k]>temperaturas[k+1]&&temperaturas[k]>temperaturas[k-1])
                     picos++;
-                else if(temp[k]<temp[k+1]&&temp[k]<temp[k-1] )
+                else if(temperaturas[k]<temperaturas[k+1]&&temperaturas[k]<temperaturas[k-1] )
                     valles++;
             }
-            System.out.println("picos = " + picos);
-            System.out.println("valles = " + valles);
+
+            System.out.println(picos + " " + valles);
+
         }
-        
     }
-    
 }
