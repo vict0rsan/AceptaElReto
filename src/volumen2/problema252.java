63f9cbcf20f91da4b8c82703ab4c70b43a7db66f
vict0rsan <vicloligye@hotmail.com>
2021-07-31 15:42:05 +0200

    Problema 252

--- src/volumen2/problema252.java
+++ src/volumen2/problema252.java
@@ -2,36 +2,38 @@ package volumen2;
 import java.util.Scanner;
 
 
-public class problema252 {
+public class problema252 { //PERFECTO - ACCEPTED
+
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-       String s;
+        String texto;
         
-        do{
-            System.out.println("Introduce un texto: ");
-            s = input.nextLine();
-            if(s.equals("XXX")==true)
+        while(true){
+
+            texto = input.nextLine();
+
+            if(texto.equals("XXX"))
                 break;
-            else
-                palindromo(s);
+
+            palindromo(texto);
             
-        }while(s.equals("XXX")!=true);
-        
-        System.out.println("PROGRAMA FINALIZADO");
+        }
     }
     
     public static void palindromo(String s){
-        String blank = s.replaceAll("\\s+", "");
-//        System.out.println("blank = " + blank);
-        int l = blank.length();
+
+        String textWihoutBlankSpaces = s.replaceAll("\\s+", "");
+        textWihoutBlankSpaces = textWihoutBlankSpaces.toLowerCase();
+        int l = textWihoutBlankSpaces.length();
         boolean check = true;
+
         for(int i = 0; i < l; i++){
-            if(blank.charAt(i)!= blank.charAt(l-i-1))
+            if(textWihoutBlankSpaces.charAt(i)!= textWihoutBlankSpaces.charAt(l-i-1))
             check = false;
         }
-        System.out.println("Palindromo: " + ((check==true)? "YES":"NO"));
-        
-        
+
+        System.out.println(check ? "SI":"NO");
     }
     
 }
