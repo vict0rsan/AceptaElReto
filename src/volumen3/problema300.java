552d92367688e3c03a19a6c83cebfce7d671bd92
vict0rsan <vicloligye@hotmail.com>
2021-07-31 16:31:07 +0200

    Problema 300

--- src/volumen3/problema300.java
+++ src/volumen3/problema300.java
@@ -2,40 +2,38 @@ package volumen3;
 
 import java.util.*;
 
-public class problema300 {
+public class problema300 { //PERFECTO - ACCEPTED
 
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         char[] vocales = new char[]{'a', 'e', 'i', 'o', 'u'};
 
-        //System.out.println("Introduce casos de prueba: ");
         int casos = input.nextInt();
         input.nextLine();
 
         for (int i = 0; i < casos; i++) {
+
             boolean[] check = new boolean[]{false, false, false, false, false};
             boolean comp = true;
-            //System.out.println("Introduce una palabra: ");
             String s = input.nextLine();
 
             for (int j = 0; j < vocales.length; j++) {
                 for (int k = 0; k < s.length(); k++) {
-                   // System.out.println("vocal en j = " + vocales[j]);
                     if (s.charAt(k) == vocales[j]) {
                         check[j] = true;
                         break;
                     }
-                    //System.out.print(s.charAt(k) + " = ");
-                    //System.out.println(vocales[j]);
                 }
             }
+
             for (int m = 0; m < check.length; m++) {
                 if (check[m] == false) {
                     comp = false;
                     break;
-//                System.out.println("check[m] = " + check[m]);
                 }
             }
+
             System.out.println(comp ? "SI" : "NO");
         }
     }
