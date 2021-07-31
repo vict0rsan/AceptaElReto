1ff57d28e2d85073243a8599ac66eb971bc8589c
vict0rsan <vicloligye@hotmail.com>
2021-07-31 15:52:35 +0200

    Problema 254

--- src/volumen2/problema254.java
+++ src/volumen2/problema254.java
@@ -1,62 +1,40 @@
 package volumen2;
+import java.util.*;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class problema254 {
+public class problema254 { //PERFECTO - ACCEPTED
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int esquiadores;
-        int suma = 0;
-        int[] prueba = {1, 2, 3, 7, 2, 54, 2, 745, 342, 12};
-        prueba = ordenador(prueba);
-        for (int i = 0; i < prueba.length; i++) {
-            System.out.println(prueba[i]);
-        }
 
-        do {
-            System.out.println("Introduce el numero de esquiadores: ");
-            esquiadores = input.nextInt();
-            if(esquiadores == 0)
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+
+            int esquiadores = input.nextInt();
+
+            if (esquiadores == 0)
                 break;
+
+            int sumaMinima = 0;
+
             int alturas[] = new int[esquiadores];
             int esquis[] = new int[esquiadores];
-            int resultado[] = new int[esquiadores];
-            for (int i = 0; i < esquiadores; i++) {
-                System.out.println("Introduce la altura del esquiador numero: " + (i + 1));
+
+            for (int i = 0; i < esquiadores; i++)
                 alturas[i] = input.nextInt();
-            }
-            for (int j = 0; j < esquiadores; j++) {
-                System.out.println("Introduce la longitud de los esquis numero: " + (j + 1));
+
+
+            for (int j = 0; j < esquiadores; j++)
                 esquis[j] = input.nextInt();
-            }
-            alturas = ordenador(alturas);
-            esquis = ordenador(esquis);
-            for (int k = 0; k < esquiadores; k++) {
-                resultado[k] = Math.abs(alturas[k] - esquis[k]);
-                suma = suma + resultado[k];
-                System.out.println("resultado[k] = " + resultado[k]);
-            }
-            System.out.println("Suma minima: " + suma);
 
-        } while (esquiadores != 0);
-    }
+            Arrays.sort(alturas);
+            Arrays.sort(esquis);
 
-    public static int[] ordenador(int[] array) {
-        int aux;
-        
-            for(int i = 0; i<array.length-1; i++){
-                for(int j = 0; j < array.length-1; j++){
-                    if(array[j]>array[j+1]){
-                        aux = array[j];
-                        array[j]=array[j+1];
-                        array[j+1]=aux;
-                    }
-                }
-            
+            for(int k = 0; k < esquiadores; k++){
+                int diferencia = Math.abs(alturas[k] - esquis[k]);
+                sumaMinima += diferencia;
+            }
+
+            System.out.println(sumaMinima);
         }
-        return array;
     }
-
 }
