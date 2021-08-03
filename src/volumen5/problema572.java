b082f83a263d531bbc0a862d298755835a52915f
vict0rsan <vicloligye@hotmail.com>
2021-08-03 13:21:29 +0200

    Problema 572

--- src/volumen5/problema572.java
+++ src/volumen5/problema572.java
@@ -1,44 +1,30 @@
 package volumen5;
+import java.util.*;
 
-import java.util.Scanner;
-
-public class problema572 {
+public class problema572 { //PERFECTO - COMPILATION ERROR POR USAR LAMBDAS
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Introduce el numero de alumnos a los que se quiere evaluar: ");
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
         int alumnos = input.nextInt();
-        double notas[];
-        boolean media = true;
 
         for (int i = 0; i < alumnos; i++) {
-            System.out.println("Introduce las notas del alumno nº: " + (i + 1));
-             notas = new double[5];
-            for (int j = 0; j < 5; j++) {
-                notas[j] = input.nextDouble();
-                //System.out.println("notas[i] = " + notas[i]);
+            List <Double> notas = new ArrayList<>();
 
-            }
-            for (int k = 0; k < 5; k++) {
-                if (notas[k] == 0) {
-                    //System.out.println(notas[k]);
-                    media = false;
-                    //System.out.println("media1 = " + media);
-                    for (int n = 0; n < 5; n++) {
-                        if (notas[n] >= 9) {
-                            media = true;
-                            //System.out.println("media2 = " + media);
-                            break;
-                        }
-                    }
-                } 
-            }
-            if (media == true) {
-            System.out.println("MEDIA");
-        } else {
-            System.out.println("SUSPENSO DIRECTO");
-        }
-        
+            for (int j = 0; j < 5; j++)
+                notas.add(input.nextDouble());
+
+            int nueve = (int) notas.stream().filter(number -> number >= 9.0).count();
+            int cero = (int) notas.stream().filter(number -> number == 0.0).count();
+
+            if ( nueve >= 1)
+                System.out.println("MEDIA");
+            else if (cero >= 1)
+                System.out.println("SUSPENSO DIRECTO");
+            else
+                System.out.println("MEDIA");
+
+            notas.clear();
         }
     }
 }
+
