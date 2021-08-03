56a8ef18feca051e5ec5de12d8d56cccf2925192
vict0rsan <vicloligye@hotmail.com>
2021-08-03 12:31:14 +0200

    Problema 538

--- src/volumen5/problema538.java
+++ src/volumen5/problema538.java
@@ -2,17 +2,21 @@ package volumen5;
 import java.util.Scanner;
 
 
-public class problema538 { //que le pasa? funciona bien pero me sale RTError
+public class problema538 { //PERFECTO - ACCEPTED
        
-         public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        final Scanner s = new Scanner(System.in);
+        final Scanner input = new Scanner(System.in);
 
         while (true) {
-            int n1 = s.nextInt();
-            int n2 = s.nextInt();
-            if (n1 == 0 && n2 == 0) break;
-            System.out.println( (n1 < n2) ? "SENIL" : "CUERDO" );
+
+            int n1 = input.nextInt();
+            int n2 = input.nextInt();
+
+            if (n1 == 0 && n2 == 0)
+                break;
+
+            System.out.println(n1 < n2 ? "SENIL" : "CUERDO" );
         }
 
     }
