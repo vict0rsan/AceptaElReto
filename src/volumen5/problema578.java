13a89e5c4077f7fd4cb0715903ff8017a8edcc5e
vict0rsan <vicloligye@hotmail.com>
2021-08-03 23:00:53 +0200

    Problema 578

--- src/volumen5/problema578.java
+++ src/volumen5/problema578.java
@@ -1,41 +1,29 @@
 package volumen5;
+import java.util.Scanner;
+import java.util.HashMap;
 
-import java.util.*;
+public class problema578 { //PERFECTO - ACCEPTED
 
-
-public class problema578 {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
         
         while(true){
             
-        HashMap<String, Integer> list = new HashMap<String, Integer>();
-        
-        int things = input.nextInt();
-        input.nextLine();
-        
-        if(things == 0)
-            break;
-        
-        String [] lista = new String[things];
-        
-        for(int i = 0; i < things; i++){
-            lista[i] = input.nextLine().toLowerCase();
-        }
-        
-        
-        
-        for(String s : lista){
-             list.put(s, 1);
-            
-        }
-        
-        
+             HashMap<String, Integer> list = new HashMap<String, Integer>();
+             int numeroProductos = input.nextInt();
+
+             if(numeroProductos == 0)
+                 break;
+
+             input.nextLine();
+
+             for(int i = 0; i < numeroProductos; i++) {
+                 String producto = input.nextLine();
+                 String productoMinusculas = producto.toLowerCase();
+                 list.put(productoMinusculas, 1);
+             }
             System.out.println(list.size());
-        
-        
+        }
     }
-    }
-    
 }
