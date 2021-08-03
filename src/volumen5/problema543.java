f26128208fe6dfde9970c0c66137349d87987ea5
vict0rsan <vicloligye@hotmail.com>
2021-08-03 12:49:44 +0200

    Problema 543

--- src/volumen5/problema543.java
+++ src/volumen5/problema543.java
@@ -1,30 +1,41 @@
 package volumen5;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class problema543 {
 
-public class problema543 { //pendiente
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int casos = s.nextInt();
-        
-        for(int i = 0; i < casos; i++){
-            int sombrillas = s.nextInt();
-            for(int j = 0; j < sombrillas; j++){
-                int x = s.nextInt();
-                int y = s.nextInt();
-                int r = s.nextInt();
-            }
-        }
-        
-    }
-    
-}
 
-class Sombrilla{
-    int x, y, r;
-    public Sombrilla(int x, int y, int r){
-        this.x = x;
-        this.y = y;
+        final Scanner s = new Scanner(System.in);
+
+        int casos = s.nextInt();
+        ArrayList<Sombrilla> sombrillas = new ArrayList<>();
+
+        while(casos-- != 0) {
+            for (int i = s.nextInt(); i > 0; i--) {
+                sombrillas.add(new Sombrilla(s.nextInt(), s.nextInt(), s.nextInt()));
+            }
+            int res = 0;
+            for (int i = 0; i < sombrillas.size(); i++) {
+                Sombrilla s1 = sombrillas.get(i);
+                for (int j = i + 1; j < sombrillas.size(); j++) {
+                    Sombrilla s2 = sombrillas.get(j);
+                    double distance = Math.sqrt(
+                            ((s1.n1 - s2.n1) * (s1.n1 - s2.n1)) + ((s1.n2 - s2.n2) * (s1.n2 - s2.n2))
+                    );
+                    if (distance < s1.r + s2.r) res++;
+                }
+            }
+            System.out.println( res );
+            sombrillas.clear();
+        }
+    }
+}
+class Sombrilla {
+    double n1, n2, r;
+    Sombrilla(int n1, int n2, int r) {
+        this.n1 = n1;
+        this.n2 = n2;
         this.r = r;
     }
 }
