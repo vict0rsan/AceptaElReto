f68e753df46038a0aa69c1fe4cba290e8dc2cb4e
vict0rsan <vicloligye@hotmail.com>
2021-08-09 13:28:27 +0200

    Problema 112

--- src/volumen1/problema112.java
+++ src/volumen1/problema112.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class problema112 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //PERFECTO - ACCEPTED
 
         Scanner input = new Scanner(System.in);
 
