4412c453c24e9021577b20715f24100e96da0f93
vict0rsan <vicloligye@hotmail.com>
2021-09-29 20:46:40 +0200

    Problema 532

--- src/volumen5/problema532.java
+++ src/volumen5/problema532.java
@@ -1,17 +1,18 @@
 package volumen5;
+
 import java.util.Scanner;
 
-public class problema532 {
+public class problema532 { //PERFECTO - ACCEPTED
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         int casos = input.nextInt();
-        
-        for( int i = 0; i < casos; i++){
+
+        for (int i = 0; i < casos; i++) {
             int nproducto = input.nextInt();
             int tproducto = input.nextInt();
-            System.out.println(tproducto - nproducto);
+            System.out.println(Math.abs(tproducto - nproducto));
         }
     }
 }
