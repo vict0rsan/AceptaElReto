dff34e154baac7e0bf8b6abdc17cc33b354e89b0
vict0rsan <vicloligye@hotmail.com>
2021-10-11 23:43:10 +0200

    Problema260

--- src/volumen2/problema260.java
+++ src/volumen2/problema260.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class problema260 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //PERFECT - ACCEPTED
 
         Scanner input = new Scanner(System.in);
 
