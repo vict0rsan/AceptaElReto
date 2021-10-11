7a59180594d6978faed3c359489a8e0675cb45b2
vict0rsan <vicloligye@hotmail.com>
2021-10-11 19:37:26 +0200

    Problema 260

---
+++ src/volumen2/problema260.java
@@ -0,0 +1,29 @@
+package volumen2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class problema260 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int cases = input.nextInt();
+        input.nextLine();
+
+        for (int i = 0; i < cases; i++) {
+            String word1 = input.nextLine();
+            String word2 = input.nextLine();
+            anagram(word1, word2);
+        }
+    }
+
+    public static void anagram (String word1, String word2){
+        char[] word_1 = word1.replaceAll("[\\s]", "").toLowerCase().toCharArray();
+        char[] word_2 = word2.replaceAll("[\\s]", "").toLowerCase().toCharArray();
+        Arrays.sort(word_1);
+        Arrays.sort(word_2);
+        System.out.println(Arrays.equals(word_1, word_2) ? "SI" : "NO");
+    }
+}
