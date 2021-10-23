1318ac873567941b764df5e064a000dc89d546c1
vict0rsan <vicloligye@hotmail.com>
2021-10-23 21:53:42 +0200

    Problema 116

--- src/volumen1/problema116.java
+++ src/volumen1/problema116.java
@@ -6,15 +6,11 @@ public class problema116 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce cuantas veces quieres que se repite la oracion 'Hola mundo': ");
+        int helloWorldTimes = input.nextInt();
 
-        int numero = input.nextInt();
-
-        if(numero >= 0 && numero <=5 ){
-        for (int i = 1; i <= numero; i++) {
-            System.out.print("Hola mundo.\n");
+        if(helloWorldTimes >= 0 && helloWorldTimes <=5 ){
+        for (int i = 1; i <= helloWorldTimes; i++)
+            System.out.println("Hola mundo.");
         }
-        }
-
     }
 }
