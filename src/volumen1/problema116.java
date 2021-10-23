663ecfa7b905b76f82ddc7a2a252499a35e0afa6
vict0rsan <vicloligye@hotmail.com>
2021-10-23 21:54:16 +0200

    Problema 116

--- src/volumen1/problema116.java
+++ src/volumen1/problema116.java
@@ -2,7 +2,7 @@ package volumen1;
 
 import java.util.Scanner;
 
-public class problema116 {
+public class problema116 { //PERFEC - ACCEPTED
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
