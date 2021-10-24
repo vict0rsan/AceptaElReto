846e98b395ea556703c0ed75e0342fcf94650584
vict0rsan <vicloligye@hotmail.com>
2021-10-24 17:12:12 +0200

    Problema 529

--- src/volumen5/problema529.java
+++ src/volumen5/problema529.java
@@ -1,28 +1,28 @@
 package volumen5;
 import java.util.Scanner;
 
-
 public class problema529 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) { //PERFECT - ACCEPTED
         Scanner input = new Scanner(System.in);
-        //System.out.println("Introduce numero de casos: ");
-        int casos = input.nextInt();
+        int cases = input.nextInt();
         input.nextLine();
-        
-        for(int i = 0; i < casos; i++){
-            int contadorI = 0;
-            int contadorO = 0;
-            String s = input.nextLine();
-            for(int j = 0; j < s.length(); j++){
-                if(s.charAt(j)=='I')
-                    contadorI++;
-                else
-                    contadorO++;
+
+        for (int i = 0; i < cases; i++) {
+            String data = input.next();
+            char[] logs = data.toCharArray();
+            int login = 0;
+            int max = 0;
+
+            for (int j = 0; j < logs.length; j++) {
+                if (logs[j] == 'I') login++;
+                if (logs[j] == 'O') {
+                    if (login > 0) login--;
+                    else max++;
+                }
+                max = Math.max(max, login);
             }
-            
-            System.out.println(contadorI>contadorO? contadorI : contadorO);
+            System.out.println( max );
         }
-        
     }
-    
 }
